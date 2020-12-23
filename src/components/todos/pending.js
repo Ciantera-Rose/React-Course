6c34dba5acc0bbf41c20bd0dc383bf433e5471e4
Ciantera-Rose <ciantera.rose@gmail.com>
2020-12-23 01:15:33 -0500

    add new file ./task-item

--- src/components/todos/pending.js
+++ src/components/todos/pending.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { TaskItem } from "./task-item";
 
 const pendingTodos = [
     { item: "task" },
@@ -9,13 +10,8 @@ const pendingTodos = [
 ]
 
 //Functionl Component that can take props
-const TaskItem = props => {
-    return ( 
-        <ul>
-            <li>{props.item}</li>
-        </ul>
-    )
-}
+//React sees this and assumes you want to create a functional component
+
 
 //Class component
 export default class Pending extends Component {
@@ -28,15 +24,17 @@ export default class Pending extends Component {
             isComplete: false
         }
     }
+    //Call functional component from within the render function
     render() {
         return ( 
         <div>
             <h2>{this.props.heading}</h2>
-            <li><TaskItem item="Some task"/></li>
+            <li><TaskItem item="Task One"/></li>
+            <li><TaskItem item="Task Two"/></li>
+            <li><TaskItem item="Task Three"/></li>
+            <li><TaskItem item="Task Four"/></li>
+            <li><TaskItem item="Task Five"/></li>
         </div> 
-
-           
-      
         )
     }
 }
---
+++ src/components/todos/task-item.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const TaskItem = props => {
+    return ( 
+        <ul>
+            <li>{props.item}</li>
+        </ul>
+    )
+}
